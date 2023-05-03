709e1427c3dda104bbd4d12371733b0ca4b86413
Shubh Arya <shubh.learner@gmail.com>
2023-05-03 12:41:34 +0530

    updates

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-import config
-
-with TelegramClient(StringSession(), config.App_api_id, config.App_api_hash) as client:
-    print(client.session.save())
+print ("hello world")
